1cc7a61568e15028dce1526cdfbca2ba023dfde3
Peter Lesko <peterlesko@yahoo.com>
2022-07-12 18:26:58 +0000

    in progress as in rec 4.7.22 3:53:00, added voting count and buttons

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,6 +1,17 @@
 const Song = (props) => {
+             //  {song : {name : songName,     singer : whoSungIt}} // this is format of the props sent from parent
+  
+  const voteForSong = () => {
+    // props.song.votes   //no ability to change like this
+    props.voteFunction(); 
+  }
+  
+  return <li>{props.song.name} by {props.song.singer} : {props.song.votes}
+      <button onClick={voteForSong}>vote</button>
+    </li>
+  
+                    // {name : songName, singer : whoSungIt}}  //this was format of the props before change
+  // return  <li>{props.name} by {props.singer}</li>;
+} 
 
-  return  <li>{props.name} by {props.singer}</li>;
-}
-
-export default Song;
+export default Song; 
--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,27 +2,9 @@ import { useState } from 'react';
 import Song from "./Song"
 
 const SongList = () => {
-  
-  // const [ulDisplayStyle, setUlDisplayStyle] = useState("none");
-  // const [buttonText,setButtonText] = useState("show songs");
 
-  // const toggleVisibility = () => {
-  //   if (ulDisplayStyle === "none") {
-  //     setUlDisplayStyle("block");
-  //     setButtonText("hide songs");
-  //     console.log("ul style:  " + ulDisplayStyle);
-  //     console.log("button:    " + buttonText);
-  //     console.log("------------------------");
-  //   }
-  //   else { 
-  //     setUlDisplayStyle("none");
-  //     setButtonText("show songs");
-  //     console.log("ul style:  " + ulDisplayStyle);
-  //     console.log("button:    " + buttonText);
-  //     console.log("------------------------");
-  //   }
-  // };
-
+  const [song1, setSong1] = useState({name : "Hooked on a feeling", singer : "Blue Suede", votes : 0});
+  const [song2, setSong2] = useState({name : "Free Bird", singer : "Leonard Skinnered", votes : 0});
 
   const [visible, setVisible] = useState(false);
 
@@ -30,17 +12,43 @@ const SongList = () => {
     setVisible(!visible);
   }
 
+  const changeSong = () => {
+    setSong1({...song1, name : "Out of the Blue"});
+  }
+
+  const voteForSong = (whichSong) => {
+    if (whichSong === 1) {
+      setSong1({...song1, votes : song1.votes + 1});
+    }
+    else {
+      setSong2({...song2, votes : song2.votes + 1});
+    }
+  }
+
   return <div>
     <h2>The songs are </h2>
-    {/* <button onClick={toggleVisibility}>{buttonText}</button> */}
     <button onClick={toggleVisibility}> {(visible) ? "hide" : "show"} songs </button>
 
-    {/* <ul style={{display: ulDisplayStyle}} >  */}
     <ul style={{display: visible ? "block" : "none"}} >
-      <Song name="Hooked on a feeling" singer="Blue Suede" />
-      <Song name="Free Bird" singer="Leonard Skinnered" />
-      </ul>
+      {/* <Song name="Hooked on a feeling" singer="Blue Suede" /> */}
+      <Song song={song1} voteFunction={() => voteForSong(1)} />
+
+      {/* <Song name="Free Bird" singer="Leonard Skinnered" /> */}
+      <Song song={song2} voteFunction={() => voteForSong(2)} />
+    </ul>
+
+    <button onClick={changeSong}>Change song 1</button>
+  
   </div>;
 }
 
 export default SongList;
+
+
+
+
+
+
+
+
+
