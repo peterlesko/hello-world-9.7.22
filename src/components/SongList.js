9473c48b0f10cfe69b09e2ff84c3db99606fec6f
Peter Lesko <peterlesko@yahoo.com>
2022-07-12 09:28:41 +0000

    completed as in rec 4.7.22 3:04:20, 2nd improved vers of the challenge-hide songs

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -3,35 +3,43 @@ import Song from "./Song"
 
 const SongList = () => {
   
-  const [ulDisplayStyle, setUlDisplayStyle] = useState("none");
-  const [buttonText,setButtonText] = useState("show songs");
+  // const [ulDisplayStyle, setUlDisplayStyle] = useState("none");
+  // const [buttonText,setButtonText] = useState("show songs");
+
+  // const toggleVisibility = () => {
+  //   if (ulDisplayStyle === "none") {
+  //     setUlDisplayStyle("block");
+  //     setButtonText("hide songs");
+  //     console.log("ul style:  " + ulDisplayStyle);
+  //     console.log("button:    " + buttonText);
+  //     console.log("------------------------");
+  //   }
+  //   else { 
+  //     setUlDisplayStyle("none");
+  //     setButtonText("show songs");
+  //     console.log("ul style:  " + ulDisplayStyle);
+  //     console.log("button:    " + buttonText);
+  //     console.log("------------------------");
+  //   }
+  // };
+
+
+  const [visible, setVisible] = useState(false);
 
   const toggleVisibility = () => {
-    if (ulDisplayStyle === "none") {
-      setUlDisplayStyle("block");
-      setButtonText("hide songs");
-      console.log("ul style:  " + ulDisplayStyle);
-      console.log("button:    " + buttonText);
-      console.log("------------------------");
-    }
-    else { 
-      setUlDisplayStyle("none");
-      setButtonText("show songs");
-      console.log("ul style:  " + ulDisplayStyle);
-      console.log("button:    " + buttonText);
-      console.log("------------------------");
-    }
-  };
+    setVisible(!visible);
+  }
 
   return <div>
     <h2>The songs are </h2>
-    <button onClick={toggleVisibility}>{buttonText}</button>
+    {/* <button onClick={toggleVisibility}>{buttonText}</button> */}
+    <button onClick={toggleVisibility}> {(visible) ? "hide" : "show"} songs </button>
 
-    <ul style={{display: ulDisplayStyle}} > 
+    {/* <ul style={{display: ulDisplayStyle}} >  */}
+    <ul style={{display: visible ? "block" : "none"}} >
       <Song name="Hooked on a feeling" singer="Blue Suede" />
       <Song name="Free Bird" singer="Leonard Skinnered" />
-      <Song name="Free Bird" singer="Leonard Skinnered" />
-    </ul>
+      </ul>
   </div>;
 }
 
